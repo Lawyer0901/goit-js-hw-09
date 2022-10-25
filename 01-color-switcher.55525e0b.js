bc1e5c9a4c4bcb7009cb7a9bf7cc055f44361dcb
Lawyer0901 <105603303+Lawyer0901@users.noreply.github.com>
2022-10-25 20:03:11 +0000

    Deploying to gh-pages from @ Lawyer0901/goit-js-hw-09@eff4c6bcae78cf10492f94c01b6be58e4277424e 🚀

--- 01-color-switcher.55525e0b.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o&&(t.disabled=!0);o=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1})),console.dir(t)}();
-//# sourceMappingURL=01-color-switcher.55525e0b.js.map
