a06411dafeab4a29bd638107c4655b8114d55810
Lawyer0901 <105603303+Lawyer0901@users.noreply.github.com>
2022-10-26 22:08:11 +0300

    some change 2 task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,6 +6,7 @@ import Notiflix from 'notiflix';
 const inputDayTime = document.querySelector('#datetime-picker');
 console.dir(inputDayTime);
 const refs = {
+  btnStart: document.querySelector('[data-start]'),
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
   minutes: document.querySelector('[data-minutes]'),
@@ -21,6 +22,31 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
 flatpickr(inputDayTime, options);
 
-console.log(options.minuteIncrement);
+refs.btnStart.addEventListener('click', stratToCountBackTime);
+
+function stratToCountBackTime(evt) {
+  console.log(evt.target);
+}
+console.log(inputDayTime.textContent);
