789da3a7b2d39fb216f8758d2b0a5148c9849af5
Lawyer0901 <105603303+Lawyer0901@users.noreply.github.com>
2022-10-30 18:48:28 +0200

    modal window done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,11 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
+// 1. Надо в функции вывода модалки описать текущую дату (selectedDates < текущей), и что ранняя дата это не правильно.
+// 2. Надо определить как в text.Content рефов присваивать разницу между выбраной датой и текущей
+// 3. Надо определить как таймер запустить в обратную сторону.
+// 4. Когда text.Content рефов = 00 то надо таймер остановить clearInterval(this.intervalId)
+
 const inputDayTime = document.querySelector('#datetime-picker');
 
 const refs = {
@@ -11,42 +16,56 @@ const refs = {
   minutes: document.querySelector('[data-minutes]'),
   seconds: document.querySelector('[data-seconds]'),
 };
-refs.btnStart.addEventListener('click', stratToCountBackTime);
 
 const timer = {
   isActive: false,
+  intervalId: null,
   start() {
     if (this.isActive) {
       return;
     }
     const sartTime = Date.now();
     this.isActive = true;
-    setInterval(() => {
+    this.intervalId = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = currentTime - sartTime;
-      const time = convertMs(deltaTime);
-      updateTimerClockFace(time);
-      //   console.log(`${days}:${hours}:${minutes}:${seconds}`);
+      const timeData = convertMs(deltaTime);
+      console.log(deltaTime);
+      updateTimerClockFace(timeData);
     }, 1000);
   },
 };
+
+// Переменная для опции в плагин
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  //   Функция вызывающая модалку ошибки выбра даты
   onClose(selectedDates) {
-    console.log(selectedDates);
-    if (!selectedDates) {
+    const currentTime = Date.now();
+    // console.log(selectedDates);
+    if (selectedDates[0] < currentTime) {
       Notiflix.Notify.warning('Please choose a date in the future');
     }
-    console.log(selectedDates[0]);
+    // console.log(selectedDates[0].getTime() - currentTime);
   },
 };
+// Eventlistener on button sart
+
+refs.btnStart.addEventListener('click', stratToCountBackTime);
+
+// Плагин выбора дня и времени
+flatpickr(inputDayTime, options);
+
+// Добавляет ноль перед цифрой, если его нет
 function pad(value) {
   return String(value).padStart(2, 0);
 }
+
+// Возвращает объек из дней часов минут и секунд
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -61,21 +80,27 @@ function convertMs(ms) {
   // Remaining minutes
   const minutes = pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  const seconds = pad(
+    Math.floor((((ms % day) % hour) % minute) / second) - second
+  );
 
   return { days, hours, minutes, seconds };
 }
 
-flatpickr(inputDayTime, options);
-
-function stratToCountBackTime(evt) {
+// Принажатии кнопки старт начинаеться отсчет таймера
+function stratToCountBackTime() {
   timer.start();
-  console.log(evt.target);
+
+  // console.log(evt.target);
 }
 
+// Присваивает значение дням часам минутам и секундам
 function updateTimerClockFace({ days, hours, minutes, seconds }) {
   refs.days.textContent = `${days}`;
   refs.hours.textContent = `${hours}`;
   refs.minutes.textContent = `${minutes}`;
   refs.seconds.textContent = `${seconds}`;
 }
+
+console.dir(flatpickr.selectedDates);
+// console.log(selectedDates.getTime());
